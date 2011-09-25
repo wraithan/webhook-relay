8e24be1b410555118fed5191c941573c70a94490
Chris McDonald <XWraithanX@gmail.com>
2011-09-25 15:04:50 -0700

    adding UserProfile

--- webhook_relay/profiles/models.py
+++ webhook_relay/profiles/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.ForeignKey('auth.User')
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, **kwargs):
+    if kwargs['created'] is True:
+        UserProfile.objects.create(user_id=kwargs['instance'].id)
